b0d967ff8bdb4999fa94f550d5ff00a5cfd59bdd
github takashicharlie <totetumonakuganbaru@gmail.com>
2020-11-05 09:35:31 +0900

    create class and component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked!")}} />
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked!")}} />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>POUPOU</div>
 }
 
 export default App;
