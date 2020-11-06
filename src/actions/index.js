e6dd08681b5b9b8c98266ccbf89ee25f5b393d52
github takashicharlie <totetumonakuganbaru@gmail.com>
2020-11-06 19:41:05 +0900

    inplement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCEREMENT = 'INCREMENT'
+export const DECEREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
+})
